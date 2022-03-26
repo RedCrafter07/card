5c8d3429153901d83b6f7948bb2802c19a6dc813
RedCrafter07 <github@redcrafter07.de>
2022-03-26 19:13:15 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+	return (
+		<div className='w-screen h-screen text-white bg-neutral-800'>
+			<p>Test</p>
+		</div>
+	);
+};
+
+export default Home;
