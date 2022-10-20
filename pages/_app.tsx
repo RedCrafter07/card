e43570e0d4b4f1f006b21d5b3916b66afdb3dd6b
RedCrafter07 <gh@r07.dev>
2022-10-20 13:22:45 +0200

    Import fontsource fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@fontsource/montserrat/900.css';
+import '@fontsource/readex-pro/300.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
